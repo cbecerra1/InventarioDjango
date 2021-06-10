d1f15438b1b8268e4f69e74b0c8af4c789249866
camilo <camilobecerra1@gmail.com>
2021-06-10 13:49:37 -0500

    Oragnizar proyecto cap34

--- appinventario/erp/views/category/views.py
+++ appinventario/erp/views/category/views.py
@@ -1,20 +1,11 @@
-from django.http.response import JsonResponse, HttpResponseRedirect
-from django.shortcuts import render, redirect #Redirect me direcciona a la pagina dependido de la url que le mande
+from django.http.response import JsonResponse
 from django.urls import reverse_lazy #con reverse_lazy tengo la ruta absoluta de esa url
 from django.utils.decorators import method_decorator
-from django.contrib.auth.decorators import login_required
-from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 from erp.models import Category, Product
 from erp.forms import *
-
-def category_list(request):
-    data = {
-        'title' : 'Listado de Categorias',
-        'categories' : Category.objects.all()
-    }
-    return render(request, 'category/list.html', data)
 
 class CategoryListView(ListView):
 
