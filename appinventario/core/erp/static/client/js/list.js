3127a594ef138512a4f86f55ee81953064ee8f7d
camilo <camilobecerra1@gmail.com>
2021-06-28 23:17:53 -0500

    Cap 50 Modal eliminar elemento

--- appinventario/core/erp/static/client/js/list.js
+++ appinventario/core/erp/static/client/js/list.js
@@ -30,7 +30,7 @@ function getData(){
                 orderable: false,
                 render: function (data, type, row) {
                     var buttons = '<a href="#" rel="edit" class="btn btn-warning btn-xs btn-flat"><i class="fas fa-edit"></i></a> ';
-                    buttons += '<a href="#" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
+                    buttons += '<a href="#" rel="delete" type="button" class="btn btn-danger btn-xs btn-flat"><i class="fas fa-trash-alt"></i></a>';
                     return buttons;
                 }
             },
@@ -55,24 +55,39 @@ $(function () {
         $('#myModalClient').modal('show'); //Inicializo mi componente modal, lo visualizamos
     });
 
-    //Para editar un elemento
-    $('#data tbody').on('click', 'a[rel="edit"]', function (){
-        modal_title.find('span').html('Edición de un cliente'); 
-        modal_title.find('i').removeClass().addClass('fas fa-edit');
-        var tr = tblClient.cell($(this).closest('td, li')).index();
-        var data = tblClient.row(tr.row).data();//Cuando haga click en una elemento de la tabla, especificmanete en el componente a y el vinculo edit va a pa sar algo
-        //TEniendo los datos los transpaso a mis componentes
-        $('input[name="action"]').val('edit');
-        $('input[name="id"]').val(data.id);
-        $('input[name="names"]').val(data.names);
-        $('input[name="surnames"]').val(data.surnames);
-        $('input[name="dni"]').val(data.dni);
-        $('input[name="date_birthday"]').val(data.date_birthday);
-        $('input[name="address"]').val(data.address);
-        $('select[name="gender"]').val(data.gender.id);
-        //Ya pase los componenest habro mi model
-        $('#myModalClient').modal('show');
-    });
+    $('#data tbody')
+        //Para editar un elemento
+        .on('click', 'a[rel="edit"]', function (){
+            modal_title.find('span').html('Edición de un cliente'); 
+            modal_title.find('i').removeClass().addClass('fas fa-edit');
+            var tr = tblClient.cell($(this).closest('td, li')).index();
+            var data = tblClient.row(tr.row).data();//Cuando haga click en una elemento de la tabla, especificmanete en el componente a y el vinculo edit va a pa sar algo
+            //TEniendo los datos los transpaso a mis componentes
+            $('input[name="action"]').val('edit');
+            $('input[name="id"]').val(data.id);
+            $('input[name="names"]').val(data.names);
+            $('input[name="surnames"]').val(data.surnames);
+            $('input[name="dni"]').val(data.dni);
+            $('input[name="date_birthday"]').val(data.date_birthday);
+            $('input[name="address"]').val(data.address);
+            $('select[name="gender"]').val(data.gender.id);
+            //Ya pase los componenest habro mi model
+            $('#myModalClient').modal('show'); 
+        })
+        //Para borrar un elemento
+        .on('click', 'a[rel="delete"]', function (){
+            var tr = tblClient.cell($(this).closest('td, li')).index();
+            var data = tblClient.row(tr.row).data();//Cuando haga click en una elemento de la tabla, especificmanete en el componente a y el vinculo edit va a pa sar algo
+            //Envio los valores por medio de ajax a mi vista
+            var parameters = new FormData();
+            //En este caso le envio valores de la siguiente manera, sin el formdata
+            parameters.append('action','delete');
+            parameters.append('id', data.id);
+            submit_with_ajax(window.location.pathname, 'Notificación', '¿Estas seguro de eliminar el siguiente registro?', parameters, function () {
+                $('#myModalClient').modal('hide');
+                tblClient.ajax.reload();
+            });
+        });
 
     //Para ocultarse el modeal y decir que va a pasar
     $('#myModalClient').on('shown.bs.modal', function () {
